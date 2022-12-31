155669b0ea742b1ca0515c30c68ef0ce2458701c
Jeffrey <jeffrey@visual-trials.io>
2022-12-31 21:10:19 +0100

    added viewpoint and from/to angle to debug

--- utils/debug.s
+++ utils/debug.s
@@ -3,6 +3,10 @@
 DEBUG_INDENT = 15
 DEBUG_TOP_MARGIN = 2
 
+debug_viewpoint_message:
+    .asciiz "Viewpoint: "
+debug_looking_dir_angle_message:
+    .asciiz "Looking dir: "
 debug_wall_index_message: 
     .asciiz "Wall index:  "
 debug_wall_start_message: 
@@ -11,26 +15,88 @@ debug_wall_end_message:
     .asciiz "Wall end:    "
 debug_screen_start_angle_message:
     .asciiz "Screen start angle: "
-debug_looking_dir_angle_message:
-    .asciiz "Looking dir angle: "
 debug_normal_distance_message:
     .asciiz "Normal distance: "
+debug_from_angle_message:
+    .asciiz "From angle: "
+debug_to_angle_message:
+    .asciiz "To angle: "
 
 
 debug_print_wall_info_on_screen:
 
+; FIXME: we should propably clear the tilemap here
+
+
     lda #COLOR_NORMAL
     sta TEXT_COLOR
     
+    lda #DEBUG_TOP_MARGIN
+    sta CURSOR_Y
     
     ; FIXME: add player position!
     
+    ; ---- Viewpoint ----
+    
+    lda #DEBUG_INDENT
+    sta CURSOR_X
+    
+    lda #<debug_viewpoint_message
+    sta TEXT_TO_PRINT
+    lda #>debug_viewpoint_message
+    sta TEXT_TO_PRINT + 1
+    
+    jsr print_text_zero
+    
+    lda VIEWPOINT_X
+    sta WORD_TO_PRINT
+    lda VIEWPOINT_X+1
+    sta WORD_TO_PRINT+1
+    
+    jsr print_fixed_point_word_as_decimal_fraction
+
+    jsr setup_cursor
+    
+    lda #':'
+    sta VERA_DATA0
+    lda TEXT_COLOR
+    sta VERA_DATA0
+    inc CURSOR_X
+    
+    lda VIEWPOINT_Y
+    sta WORD_TO_PRINT
+    lda VIEWPOINT_Y+1
+    sta WORD_TO_PRINT+1
+    
+    jsr print_fixed_point_word_as_decimal_fraction
+    
+    inc CURSOR_Y
+    
+    ; ---- Looking dir angle ----
+    
+    lda #DEBUG_INDENT
+    sta CURSOR_X
+    
+    lda #<debug_looking_dir_angle_message
+    sta TEXT_TO_PRINT
+    lda #>debug_looking_dir_angle_message
+    sta TEXT_TO_PRINT + 1
+    
+    jsr print_text_zero
+
+    lda LOOKING_DIR_ANGLE
+    sta WORD_TO_PRINT
+    lda LOOKING_DIR_ANGLE+1
+    sta WORD_TO_PRINT+1
+    
+    jsr print_word_as_decimal
+    
+    inc CURSOR_Y
+    inc CURSOR_Y
     
     ; ---- Wall index ----
     lda #DEBUG_INDENT
     sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+0
-    sta CURSOR_Y
     
     lda #<debug_wall_index_message
     sta TEXT_TO_PRINT
@@ -45,12 +111,12 @@ debug_print_wall_info_on_screen:
 
     jsr print_byte_as_decimal
     
+    inc CURSOR_Y
+    
     ; ---- Wall start ----
 
     lda #DEBUG_INDENT
     sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+1
-    sta CURSOR_Y
     
     lda #<debug_wall_start_message
     sta TEXT_TO_PRINT
@@ -77,12 +143,12 @@ debug_print_wall_info_on_screen:
     
     jsr print_byte_as_decimal
     
+    inc CURSOR_Y
+    
     ; ---- Wall end ----
 
     lda #DEBUG_INDENT
     sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+2
-    sta CURSOR_Y
     
     lda #<debug_wall_end_message
     sta TEXT_TO_PRINT
@@ -109,35 +175,12 @@ debug_print_wall_info_on_screen:
     
     jsr print_byte_as_decimal
     
-    
-    ; ---- Looking dir angle ----
-    
-    
-    lda #DEBUG_INDENT
-    sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+3
-    sta CURSOR_Y
-    
-    lda #<debug_looking_dir_angle_message
-    sta TEXT_TO_PRINT
-    lda #>debug_looking_dir_angle_message
-    sta TEXT_TO_PRINT + 1
-    
-    jsr print_text_zero
-
-    lda LOOKING_DIR_ANGLE
-    sta WORD_TO_PRINT
-    lda LOOKING_DIR_ANGLE+1
-    sta WORD_TO_PRINT+1
-    
-    jsr print_word_as_decimal
+    inc CURSOR_Y
     
     ; ---- Screen start angle ----
     
     lda #DEBUG_INDENT
     sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+4
-    sta CURSOR_Y
     
     lda #<debug_screen_start_angle_message
     sta TEXT_TO_PRINT
@@ -153,13 +196,12 @@ debug_print_wall_info_on_screen:
     
     jsr print_word_as_decimal
     
+    inc CURSOR_Y
     
     ; ---- Screen normal distance ----
     
     lda #DEBUG_INDENT
     sta CURSOR_X
-    lda #DEBUG_TOP_MARGIN+5
-    sta CURSOR_Y
     
     lda #<debug_normal_distance_message
     sta TEXT_TO_PRINT
@@ -175,5 +217,48 @@ debug_print_wall_info_on_screen:
     
     jsr print_fixed_point_word_as_decimal_fraction
     
+    inc CURSOR_Y
+    
+    ; ---- From angle ----
+    
+    lda #DEBUG_INDENT
+    sta CURSOR_X
+    
+    lda #<debug_from_angle_message
+    sta TEXT_TO_PRINT
+    lda #>debug_from_angle_message
+    sta TEXT_TO_PRINT + 1
+    
+    jsr print_text_zero
+
+    lda FROM_ANGLE
+    sta WORD_TO_PRINT
+    lda FROM_ANGLE+1
+    sta WORD_TO_PRINT+1
+    
+    jsr print_word_as_decimal
+    
+    inc CURSOR_Y
+    
+    ; ---- To angle ----
+    
+    lda #DEBUG_INDENT
+    sta CURSOR_X
+    
+    lda #<debug_to_angle_message
+    sta TEXT_TO_PRINT
+    lda #>debug_to_angle_message
+    sta TEXT_TO_PRINT + 1
+    
+    jsr print_text_zero
+
+    lda TO_ANGLE
+    sta WORD_TO_PRINT
+    lda TO_ANGLE+1
+    sta WORD_TO_PRINT+1
+    
+    jsr print_word_as_decimal
+    
+    inc CURSOR_Y
     
     rts
