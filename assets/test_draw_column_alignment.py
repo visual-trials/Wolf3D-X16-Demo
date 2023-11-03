966eb4147693b90d34341911532e16ab430b7a51
Jeffrey <jeffrey@visual-trials.io>
2023-11-03 17:53:10 +0100

    comparing alignment of two types of column drawers

--- assets/test_draw_column_alignment.py
+++ assets/test_draw_column_alignment.py
@@ -18,6 +18,9 @@ orange_color = (200,100,0)
 screen_width = 320
 screen_height = 240
 
+left_border = 25
+top_border = 20
+
 pygame.init()
 
 pygame.display.set_caption('X16 Wold3D TEST draw column alignment')
@@ -27,10 +30,14 @@ clock = pygame.time.Clock()
 
 column_texture = []
 for i in range(64):
-    if (i % 2 == 0):
+    if (i % 4 == 0):
         column_texture.append(yellow_color)
-    else:
+    elif (i % 4 == 1):
         column_texture.append(red_color)
+    elif (i % 4 == 2):
+        column_texture.append(green_color)
+    elif (i % 4 == 3):
+        column_texture.append(blue_color)
 
 
 def run():
@@ -66,7 +73,7 @@ def run():
         wall_height = 64
         screen_height = 152
         draw_single_wall_column(screen, x, wall_height, screen_height, column_texture)
-        # FIXME: draw_column()
+        draw_single_wall_column_new(screen, x+1, wall_height, screen_height, column_texture)
     
         
         pygame.display.update()
@@ -79,8 +86,10 @@ def run():
 
 def draw_single_wall_column(screen, x, wall_height, screen_height, column_texture):
 
-    left_border = 25
-    top_border = 20
+    # TEST: how to setup these number correctly?
+    texture_row_index = 0.5  
+    # increment_texture = 0.8
+    increment_texture = 0.7
 
     for y in range(wall_height):
         # FIXME: y should be a VIRTUAL y-position and we should skip certain y-coordinates
@@ -88,14 +97,47 @@ def draw_single_wall_column(screen, x, wall_height, screen_height, column_textur
         # FIXME: we should probably change x_screen too?
         x_screen = x + left_border
         
-        # FIXME: do some form of SCALING here!!
-        row_index = y
-        
-        pixel_color = column_texture[row_index]
+        pixel_color = column_texture[int(texture_row_index)]
         
         # FIXME: we are doing *6 here, which is overkill
         pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*6, y_screen*6, 6, 6))
 
+        # FIXME: do some form of SCALING here!!
+        texture_row_index += increment_texture 
+        
+
+
+def draw_single_wall_column_new(screen, x, wall_height, screen_height, column_texture):
+
+    # TEST: how to setup these number correctly?
+    screen_row_index = 0.49  
+    # increment_texture = 0.8
+    increment_texture = 0.7
     
+    increment = (1/increment_texture)/2
+
+    texture_height = 64
+    for y_texture in range(texture_height):
+        # FIXME: we should probably change x_screen too?
+        x_screen = x + left_border
+        
+        pixel_color = column_texture[y_texture]
+        
+        # FIXME: y should be a VIRTUAL y-position and we should skip certain y-coordinates
+        y_screen = int(screen_row_index) + top_border
+        # FIXME: we are doing *6 here, which is overkill
+        pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*6, y_screen*6, 6, 6))
+        screen_row_index += increment 
+        
+        if (screen_row_index > wall_height):
+            break
+            
+        y_screen = int(screen_row_index) + top_border
+        # FIXME: we are doing *6 here, which is overkill
+        pygame.draw.rect(screen, pixel_color, pygame.Rect(x_screen*6, y_screen*6, 6, 6))
+        screen_row_index += increment 
+        
+        if (screen_row_index > wall_height):
+            break
     
 run()
